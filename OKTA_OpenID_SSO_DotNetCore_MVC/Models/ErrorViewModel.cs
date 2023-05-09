a9656357fe0a6bbc5446f2e153ff356fd220ccc0
Adinarayana Reddy JC <jc.adi101@gmail.com>
2023-05-09 15:44:15 +0530

    Add project files.

---
+++ OKTA_OpenID_SSO_DotNetCore_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OKTA_OpenID_SSO_DotNetCore_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
